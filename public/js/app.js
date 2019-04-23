932d8064601577a98e86b1e7140ab7bdde08c591
Vivek <vivekssharma07@gmail.com>
2019-04-23 18:41:07 +0530

    minor chnages

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,8 @@ formSubmit.addEventListener('submit',(e) => {
             response.json().then((data) => {
                     if(data.error){
                         //console.log(data.error);
-                        messageOne.textContent = data.error;                   
+                        messageOne.textContent = data.error;   
+                        messageTwo.textContent = '';                
                     } else {
                         messageOne.textContent = data.location;
                         messageTwo.textContent = data.forecast;
